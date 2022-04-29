1ad3af7fcc9610d723d1c9db6b7865aa1a201664
ByeongSu-Koo <bitter0061@gmail.com>
2022-04-29 09:51:20 +0900

    MemberVO

--- webstudy777-ksj/src/main/java/org/kosta/myproject/model/MemberVO.java
+++ webstudy777-ksj/src/main/java/org/kosta/myproject/model/MemberVO.java
@@ -1,5 +1,21 @@
 package org.kosta.myproject.model;
 
 public class MemberVO {
+	private String id;
+	private String name;
+	private String age;
+	public MemberVO(String id, String name, String age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+	public MemberVO() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "MemberVO [id=" + id + ", name=" + name + ", age=" + age + "]";
+	}
 
 }
