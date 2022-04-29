b7c87c237addb519b0c123ee6d8f72290b14bbc9
yeonho-oh <ohdusho@gmail.com>
2022-04-29 10:22:37 +0900

    new member vo

--- webstudy777-ksj/src/main/java/org/kosta/myproject/model/MemberVO.java
+++ webstudy777-ksj/src/main/java/org/kosta/myproject/model/MemberVO.java
@@ -4,43 +4,52 @@ public class MemberVO {
 	private String id;
 	private String name;
 	private String age;
+	private String password;
+	public MemberVO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public MemberVO(String id, String name, String age, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.password = password;
+	}	
 	public MemberVO(String id, String name, String age) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.age = age;
 	}
-	
 	public String getId() {
 		return id;
 	}
-
 	public void setId(String id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getAge() {
 		return age;
 	}
-
 	public void setAge(String age) {
 		this.age = age;
 	}
-
-	public MemberVO() {
-		super();
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
 	}
 	@Override
 	public String toString() {
-		return "MemberVO [id=" + id + ", name=" + name + ", age=" + age + "]";
+		return "MemberVO [id=" + id + ", name=" + name + ", age=" + age + ", password=" + password + "]";
 	}
+	
 
 }
