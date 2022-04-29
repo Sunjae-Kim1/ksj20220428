851e272ded3487d5373e3d448b995466d585979c
sunjae-kim1 <ksj100301@gmail.com>
2022-04-29 10:22:49 +0900

    Merge branch 'main' of https://github.com/Sunjae-Kim1/ksj20220428.git into main

--- webstudy777-ksj/src/main/java/org/kosta/myproject/model/MemberVO.java
+++ webstudy777-ksj/src/main/java/org/kosta/myproject/model/MemberVO.java
@@ -1,6 +1,5 @@
 package org.kosta.myproject.model;
 
-<<<<<<< HEAD
 import java.io.Serializable;
 
 public class MemberVO implements Serializable{
@@ -48,49 +47,4 @@ public class MemberVO implements Serializable{
 		return "MemberVO [id=" + id + ", password=" + password + ", name=" + name + ", age=" + age + "]";
 	}
 }
-=======
-public class MemberVO {
-	private String id;
-	private String name;
-	private String age;
-	public MemberVO(String id, String name, String age) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.age = age;
-	}
-	
-	public String getId() {
-		return id;
-	}
 
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getAge() {
-		return age;
-	}
-
-	public void setAge(String age) {
-		this.age = age;
-	}
-
-	public MemberVO() {
-		super();
-	}
-	@Override
-	public String toString() {
-		return "MemberVO [id=" + id + ", name=" + name + ", age=" + age + "]";
-	}
-
-}
->>>>>>> branch 'main' of https://github.com/Sunjae-Kim1/ksj20220428.git
