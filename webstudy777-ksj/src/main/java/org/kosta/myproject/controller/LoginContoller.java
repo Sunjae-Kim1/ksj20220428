40d549a21d1789a3f607d9b51756376b35c7a4d6
Jihoon-Shim <tlawnsdl37@gmail.com>
2022-05-02 09:37:30 +0900

    LoginController

--- webstudy777-ksj/src/main/java/org/kosta/myproject/controller/LoginContoller.java
+++ webstudy777-ksj/src/main/java/org/kosta/myproject/controller/LoginContoller.java
@@ -2,6 +2,7 @@ package org.kosta.myproject.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.kosta.myproject.model.MemberDAO;
 import org.kosta.myproject.model.MemberVO;
@@ -13,12 +14,15 @@ public class LoginContoller implements Controller {
 		String id = request.getParameter("id");
 		String pw = request.getParameter("password");
 		MemberVO vo = MemberDAO.getInstance().login(id, pw);
+		String viewName = null;
 		if(vo==null) {
-			
+			viewName="member/login-fail.jsp";
 		}else {
-			
+			HttpSession session = request.getSession();
+			session.setAttribute("memberVO", vo);
+			viewName = "redirect:FilmController.do";
 		}
-		return null;
+		return viewName;
 	}
 
 }
